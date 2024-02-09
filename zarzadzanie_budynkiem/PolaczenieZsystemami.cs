bc38ff7b134c658c08f9f961b5eeb49b33462753
Cristiano <121246015+Krystian-Pedziwiatr@users.noreply.github.com>
2024-02-09 09:51:52 +0100

    Add files via upload

---
+++ zarzadzanie_budynkiem/PolaczenieZsystemami.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+
+namespace zarzadzanie_budynkiem
+{
+    public class PolaczenieZsystemami
+    {
+        public PolaczenieZsystemami()
+        {
+            public void WyswietlPolacznia()
+            {
+                Console.WriteLine("Aby połączyć się z innym urządzeniem wyszukaj 'System Zarządzania Budynku' w swojej sieci lokalnej a następnie wpisz poniższy kod:");
+                Console.WriteLine("778901");
+
+            }
+        }
+    }
+
+
+}
